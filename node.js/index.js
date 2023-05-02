f9df65b507047bae5d3d2ae4d2ad2d527a207e00
sukalov <matvey10@gmail.com>
2023-05-02 19:56:01 +0300

    CORS needed (implementing backend)

---
+++ node.js/index.js
@@ -0,0 +1,79 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+app.use(express.json())
+
+let notes = [
+{
+      "id": 1,
+      "content": "HTML is easy",
+      "important": true
+    },
+    {
+      "id": 2,
+      "content": "Browser can execute only JavaScript",
+      "important": true
+    },
+    {
+      "id": 3,
+      "content": "GET and POST are the most important methods of HTTP protocol",
+      "important": true
+    },
+    {
+      "content": "Yo, we're throwing data to the server!",
+      "important": false,
+      "id": 4
+    }
+  ]
+
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h>')
+})
+app.get('/api/notes', (request, response) => {
+    response.json(notes)
+})
+app.get('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const note = notes.find(note => note.id === id)
+    if (note) {
+    response.json(note)
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    notes = notes.filter(note => note.id !== id)
+    response.status(204).end()
+})
+
+const generateId = () => {
+    const maxId = notes.length > 0
+        ? matchMedia.max(...notes.map(n => n.id))
+        : 0
+    return maxId + 1
+}
+
+app.post('/api/notes', (request, response) => {
+    const body = request.body()
+
+    if (!body.content) {
+        return response.status(400).json({
+            error: 'content missing ('
+        })
+    }
+
+    const note = {
+        content: body.content,
+        important: body.important || false,
+        id: generateId()
+    }
+
+    notes = mnotes.concat(note)
+    response.json(note)
+})
+
+const PORT = 3001
+app.listen(PORT, () => console.log(`Express server running on port ${PORT}`))
